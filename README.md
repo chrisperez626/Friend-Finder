f51c15c927daee5b16ad4046cd7d82cb7135ee2e
chrisperez626 <chrisperez626@gmail.com>
2018-04-20 12:52:21 -0400

    initial program files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Friend-Finder
+
+## Link to Heroku App: https://intense-tundra-55888.herokuapp.com/
+
+Upon opening the website the user will be prompted with a home screen. There will then be the option to 
+either go straight to the survey or to choose view the API of the friends that are available. Once completed,
+the survey will be entered into the server which will then pop out the person who you are most compatible with.
+
+### Enjoy and feel free to add if not yourself some other interesting characters
--- app/data/friends.js
+++ app/data/friends.js
@@ -11,10 +11,11 @@ var friendsArray = [
 		"4",
 		"2",
 		"2",
-		"5"]
+		"5",
+		"2"]
 	},{
-		name: "Christian",
-		picture: "https://avatars3.githubusercontent.com/u/34457915?s=460&v=4",
+		name: "Mike",
+		picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Jordan_Lipofsky.jpg/170px-Jordan_Lipofsky.jpg",
 		score: [
 		"5",
 		"5",
@@ -24,10 +25,11 @@ var friendsArray = [
 		"4",
 		"2",
 		"2",
-		"5"]
+		"5",
+		"3"]
 	},{
-		name: "Christian",
-		picture: "https://avatars3.githubusercontent.com/u/34457915?s=460&v=4",
+		name: "Scott",
+		picture: "https://www.billboard.com/files/styles/article_main_image/public/media/Kid-Cudi-beat-bb11-2016-billboard-650.jpg",
 		score: [
 		"4",
 		"5",
@@ -37,10 +39,11 @@ var friendsArray = [
 		"4",
 		"2",
 		"2",
-		"5"]
+		"5",
+		"4"]
 	},{
-		name: "Christian",
-		picture: "https://avatars3.githubusercontent.com/u/34457915?s=460&v=4",
+		name: "Quavious",
+		picture: "http://s3.amazonaws.com/hiphopdx-production/2017/11/171106-Quavo-DX-Mike-Lavin-800x600.jpg",
 		score: [
 		"3",
 		"5",
@@ -50,10 +53,11 @@ var friendsArray = [
 		"4",
 		"2",
 		"2",
-		"5"]
+		"5",
+		"1"]
 	},{
-		name: "Christian",
-		picture: "https://avatars3.githubusercontent.com/u/34457915?s=460&v=4",
+		name: "Odell",
+		picture: "https://usatgiantswire.files.wordpress.com/2016/08/usatsi_8152454.jpg?w=1000&h=600&crop=1",
 		score: [
 		"2",
 		"5",
@@ -63,7 +67,8 @@ var friendsArray = [
 		"4",
 		"2",
 		"2",
-		"5"]
+		"5",
+		"4"]
 	},
 
 ]
